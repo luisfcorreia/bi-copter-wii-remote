cb4aa649e13bd62805d0397502d1296008953f90
Luis Correia <buga@loide.net>
2011-09-30 23:14:59 +0100

    Bluetooth tests

---
+++ src/net/loide/games/bicopter/Bluetest.java
@@ -0,0 +1,52 @@
+package net.loide.games.bicopter;
+
+
+import fi.sulautetut.android.tblueclient.TBlue;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Bluetest extends Activity {
+ 
+    TBlue tBlue;
+    TextView messagesTv; 
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initGUI();
+    }
+ 
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        tBlue=new TBlue("08:11:07:08:00:17"); 
+        if (tBlue.streaming()) {
+            messagesTv.append("Connected succesfully! ");
+        } else {
+            messagesTv.append("Error: Failed to connect. ");
+        } 
+        String s="";
+        while (tBlue.streaming() && (s.length()<10) ) {
+            s+=tBlue.read();
+        }
+        messagesTv.append("Read from Bluetooth: \n"+s);
+    }
+ 
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        tBlue.close();
+    } 
+ 
+    public void initGUI()
+    {
+        LinearLayout container=new LinearLayout(this);
+        messagesTv = new TextView(this);
+        container.addView(messagesTv);
+        setContentView(container); 
+    }
+}
--- src/net/loide/games/bicopter/MultiWiiBT_menu.java
+++ src/net/loide/games/bicopter/MultiWiiBT_menu.java
@@ -2,8 +2,10 @@ package net.loide.games.bicopter;
 
 import java.util.List;
 
+
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
@@ -17,18 +19,18 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MultiWiiBT_menu extends Activity implements OnClickListener {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
 
-        SensorManager mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		SensorManager mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 		List<Sensor> mySensors = mySensorManager
 				.getSensorList(Sensor.TYPE_ORIENTATION);
 
@@ -48,44 +50,42 @@ public class MultiWiiBT_menu extends Activity implements OnClickListener {
 			sensorrunning = false;
 			finish();
 		}
-		
+
 		Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 		vibrator.cancel();
-		
 
 		Button button1 = (Button) findViewById(R.id.startgameBtn);
 		Button button3 = (Button) findViewById(R.id.exitBtn);
 
-//		button1.setVisibility(View.INVISIBLE);
+		// button1.setVisibility(View.INVISIBLE);
 
 		button1.setOnClickListener(this);
 		button3.setOnClickListener(this);
-    
-    
-    }
-    
-    
+
+	}
+
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.startgameBtn:
 			// handle button A click;
 
-//			if (scannedText.compareTo("") == 0) {
-				Toast.makeText(this,
-						"Primeiro tem que ler o código de barras!",
-						Toast.LENGTH_LONG).show();
-				break;
-/*
-			} else {
-				
-				// enviar mensagem para o servidor passar ao estado Jogo
-				
-  				Intent myIntent = new Intent(Entrymenu.this, Battlefield.class);
-				Entrymenu.this.startActivity(myIntent);
-				break;
-			}
-*/
+			Intent myIntent = new Intent(MultiWiiBT_menu.this, Bluetest.class);
+			MultiWiiBT_menu.this.startActivity(myIntent);
+			break;
 
+		/*
+		 * 
+		 * if (scannedText.compareTo("") == 0) { Toast.makeText(this,
+		 * "Primeiro tem que ler o código de barras!",
+		 * Toast.LENGTH_LONG).show();
+		 * 
+		 * } else {
+		 * 
+		 * // enviar mensagem para o servidor passar ao estado Jogo
+		 * 
+		 * Intent myIntent = new Intent(Entrymenu.this, Battlefield.class);
+		 * Entrymenu.this.startActivity(myIntent); break; }
+		 */
 
 		case R.id.exitBtn:
 			// handle button B click;
@@ -93,6 +93,5 @@ public class MultiWiiBT_menu extends Activity implements OnClickListener {
 			break;
 		}
 	}
-    
-    
+
 }
