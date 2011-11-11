7b8c23c8c07e5ddaaf5c06b7c13cc6ffcb914f39
Rafael Pato <xmetalweirdox@gmail.com>
2011-11-11 01:51:10 +0000

    Full UI change

---
+++ src/net/loide/games/bicopter/Controller.java
@@ -0,0 +1,300 @@
+package net.loide.games.bicopter;
+
+import fi.sulautetut.android.tblueclient.TBlue;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.util.Log;
+
+public class Controller extends Activity implements OnTouchListener,
+		SensorEventListener {
+
+	// Unique UUID for this application
+	private SensorManager sensorManager = null;
+	private TBlue bt;
+	private String bt_rd = "";
+	private String mac = "";
+	private boolean running = false;
+	private int BT_SEND_DELAY = 10;
+
+	private int circleSize = 32;
+	private float lX = 150;
+	private float lY = 480 - circleSize;
+
+	public int mThr = 50;
+	public int mYaw = 50;
+	public int mPit = 50;
+	public int mRol = 50;
+	public int mAux = 50;
+	public float base_mPit = 0;
+	public float base_mRol = 0;
+	public int pitch;
+	public int roll;
+	public int arm = 0;
+	public int prearm = 0;
+    String TAG="Controller";
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(new MyView(this));
+
+		mPaint = new Paint();
+		mPaint.setAntiAlias(true);
+		mPaint.setDither(true);
+		mPaint.setColor(0xFFFF0000);
+		mPaint.setStrokeWidth(4);
+
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+		mac = MultiWiiBT_menu.remote_device_mac;
+		bt = new TBlue(mac);
+
+		running = true;
+		CommLink.run();
+	}
+
+	private Paint mPaint;
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		return false;
+	}
+
+	public class MyView extends View {
+
+		private Canvas mCanvas;
+		private float x = 0, y = 0;
+		private float dx1 = 0, dy1 = 0;
+		private static final float TOUCH_TOLERANCE = 4;
+
+		public MyView(Context c) {
+			super(c);
+		}
+
+		public void onCreate() {
+			android.graphics.Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+			Bitmap mBitmap = Bitmap.createBitmap(480, 800, conf);
+			setmCanvas(new Canvas(mBitmap));
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+
+			// desenhar quadrado esquerdo
+			mPaint.setColor(0xFF92C957);
+			canvas.drawRect(0, 0, 300, 480, mPaint);
+
+			// desenhar quadrado esquerdo
+			mPaint.setColor(0xFF000066);
+			canvas.drawRect(301, 0, 800, 60, mPaint);
+
+			// desenhar app name
+			mPaint.setColor(0xFF00FFFF);
+			mPaint.setTextSize(42);
+			canvas.drawText("MultiWiiBT UI v" + MultiWiiBT_menu.UI_VERSION,
+					360, 45, mPaint);
+
+			// desenhar arm/disarm button
+			mPaint.setColor(0xFF660000);
+			canvas.drawRect(600, 70, 800, 130, mPaint);
+			mPaint.setTextSize(42);
+			mPaint.setColor(0xFF00FF00);
+			if (arm == 1) {
+				canvas.drawText("-DISARM-", 610, 115, mPaint);
+			} else {
+				canvas.drawText("--ARM---", 610, 115, mPaint);
+			}
+
+			mPaint.setTextSize(20);
+			mPaint.setColor(0xFFFFFFFF);
+			canvas.drawText("Pitch  : " + pitch, 310, 120, mPaint);
+			canvas.drawText("Roll    : " + roll, 310, 140, mPaint);
+			
+			canvas.drawText("BlueT : " + bt_rd, 310, 240, mPaint);
+
+			// desenhar stick esquerdo
+			mPaint.setColor(0xFFCDE3A1);
+			canvas.drawCircle(lX, lY, circleSize, mPaint);
+
+			// desenhar simulador de sticks
+			mPaint.setColor(0xFFAAAAAA);
+			mPaint.setStyle(Paint.Style.STROKE);
+			canvas.drawRect(360, 260, 560, 460, mPaint);
+			canvas.drawLine(460, 260, 460, 460, mPaint);
+			canvas.drawLine(360, 360, 560, 360, mPaint);
+			canvas.drawRect(580, 260, 780, 460, mPaint);
+			canvas.drawLine(680, 260, 680, 460, mPaint);
+			canvas.drawLine(580, 360, 780, 360, mPaint);
+
+			// desenhar posição calculada dos sticks
+			mPaint.setStyle(Paint.Style.FILL);
+			mPaint.setColor(0xFFCDE3A1);
+			canvas.drawCircle(360 + (mYaw * 2),
+					Math.abs(260 + (200 - (mThr * 2))), circleSize / 2, mPaint);
+			canvas.drawCircle(580 + (mRol * 2),
+					Math.abs(260 + (200 - (mPit * 2))), circleSize / 2, mPaint);
+		}
+
+		public boolean onTouchEvent(MotionEvent event) {
+
+			x = event.getX();
+			y = event.getY();
+
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				if ((x > 1) && (x < 300)) {
+					if ((y > 1) && (y < 480)) {
+						lX = x;
+						lY = y;
+						base_mPit = pitch;
+						base_mRol = roll;
+					}
+				}
+				if ((x > 600) && (x < 800)) {
+					if ((y > 70) && (y < 130)) {
+						prearm = 1;
+					}
+				}
+				invalidate();
+				break;
+
+			case MotionEvent.ACTION_MOVE:
+				int maxP;
+				int maxR;
+				int fifty = 50;
+				if ((x > 1) && (x < 300)) {
+					if ((y > 1) && (y < 480)) {
+						dx1 = Math.abs(x - lX);
+						dy1 = Math.abs(y - lY);
+						if (dx1 >= TOUCH_TOLERANCE || dy1 >= TOUCH_TOLERANCE) {
+							lX = x;
+							lY = y;
+							maxP = (int) (base_mPit - pitch);
+							maxR = (int) (base_mRol - roll);
+							if (maxP > fifty) {
+								maxP = fifty;
+							}
+							if (maxP < -fifty) {
+								maxP = -fifty;
+							}
+							if (maxR > fifty) {
+								maxR = fifty;
+							}
+							if (maxR < -fifty) {
+								maxR = -fifty;
+							}
+							mPit = maxP + fifty;
+							mRol = maxR + fifty;
+						}
+					}
+				}
+				invalidate();
+				break;
+
+			case MotionEvent.ACTION_UP:
+				lX = 150;
+				base_mPit = 50;
+				base_mRol = 50;
+				mPit = 50;
+				mRol = 50;
+				mYaw = 50;
+
+				if ((prearm == 1) && (x > 600) && (x < 800)) {
+					if ((y > 70) && (y < 130)) {
+						prearm = 0;
+						if (arm == 0) {
+							arm = 1;
+						} else {
+							arm = 0;
+						}
+					}
+				}
+				invalidate();
+				break;
+			}
+			invalidate();
+			return true;
+		}
+
+		public void setmCanvas(Canvas mCanvas) {
+			this.mCanvas = mCanvas;
+		}
+
+		public Canvas getmCanvas() {
+			return mCanvas;
+		}
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// Register this class as a listener for the accelerometer sensor
+		sensorManager.registerListener(this,
+				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+				SensorManager.SENSOR_DELAY_GAME);
+		// ...and the orientation sensor
+		sensorManager.registerListener(this,
+				sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+				SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	@Override
+	protected void onStop() {
+		// Unregister the listener
+		sensorManager.unregisterListener(this);
+		running = false;
+		bt.close();
+		super.onStop();
+	}
+
+	public void onSensorChanged(SensorEvent sensorEvent) {
+		if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+			roll = Math.round(sensorEvent.values[1]);
+			pitch = Math.round(sensorEvent.values[2]);
+		}
+	}
+
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+	}
+
+	private Handler mHandler = new Handler();
+	private Runnable CommLink = new Runnable() {
+		private String data = "";
+
+		public void run() {
+			int mt, mr, mp, my, ma;
+			if (running) {
+
+				mThr = (int) (Math.abs(lY - 480) * 99 / 480) + 1;
+				mYaw = (int) (lX * 99 / 300) + 1;
+				mAux = (int) (arm * 99) + 1;
+				mt = mThr * 255 / 100;
+				mr = mRol * 255 / 100;
+				mp = mPit * 255 / 100;
+				my = mYaw * 255 / 100;
+				ma = mAux * 255 / 100;
+
+				data = "K" + (char) mt + (char) mr + (char) mp + (char) my
+						+ (char) ma;
+				bt.write(data);
+				bt_rd = bt.read();
+
+				Log.i(TAG, "Throttle:"+mThr+" Yaw:"+mYaw+" Roll:"+mRol+" Pitch:"+mPit+" Aux1:"+mAux);
+				mHandler.postDelayed(CommLink, BT_SEND_DELAY);
+			}
+		}
+	};
+
+}
---
+++ src/net/loide/games/bicopter/MultiWiiBT_menu.java
@@ -0,0 +1,173 @@
+package net.loide.games.bicopter;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MultiWiiBT_menu extends Activity implements OnClickListener {
+
+	public static String remote_device_mac = "";
+	public static String MY_PREFS_FILE_NAME = "net.loide.games.bicopter.multiwiibt.conf";
+	public static SharedPreferences prefs;
+	public static String UI_VERSION = "0.2";
+
+	// Intent request codes
+	private static final int REQUEST_CONNECT_DEVICE = 1;
+	private static final int REQUEST_ENABLE_BT = 2;
+
+	private Vibrator vibrator = null;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		prefs = new ObscuredSharedPreferences(this, this.getSharedPreferences(
+				MY_PREFS_FILE_NAME, Context.MODE_PRIVATE));
+		/*
+		 * example new value prefs.edit().putString("foo", "bar").commit();
+		 */
+		remote_device_mac = prefs.getString("remote_device", "");
+
+		if (remote_device_mac != "") {
+			Toast.makeText(
+					this,
+					"Bluetooth device " + remote_device_mac
+							+ " read from config file.", Toast.LENGTH_SHORT)
+					.show();
+		}
+
+		Button button1 = (Button) findViewById(R.id.startgameBtn);
+		Button button2 = (Button) findViewById(R.id.configBtn);
+		Button button3 = (Button) findViewById(R.id.exitBtn);
+
+		button1.setOnClickListener(this);
+		button2.setOnClickListener(this);
+		button3.setOnClickListener(this);
+
+		// init sensors
+		vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		vibrator.cancel();
+
+		// Get local Bluetooth adapter
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter
+				.getDefaultAdapter();
+
+		// If the adapter is null, then Bluetooth is not supported
+		if (mBluetoothAdapter == null) {
+			Toast.makeText(this, "Bluetooth is not available",
+					Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		}
+
+		// If BT is not on, request that it be enabled.
+		if (!mBluetoothAdapter.isEnabled()) {
+			Intent enableIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+		}
+	}
+
+	public void onClick(View v) {
+		Intent myIntent;
+		switch (v.getId()) {
+		case R.id.startgameBtn:
+			if (remote_device_mac == "") {
+				Toast.makeText(this, "Seleccione um dispositivo Bluetooth!",
+						Toast.LENGTH_LONG).show();
+			} else {
+
+				myIntent = new Intent(MultiWiiBT_menu.this, Controller.class);
+				MultiWiiBT_menu.this.startActivity(myIntent);
+			}
+			break;
+
+		case R.id.configBtn:
+			 
+			 myIntent = new Intent(MultiWiiBT_menu.this, Config.class);
+			 MultiWiiBT_menu.this.startActivity(myIntent);
+			break;
+
+		case R.id.exitBtn:
+			// handle button B click;
+			MultiWiiBT_menu.this.finish();
+			break;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.option_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.scan:
+			Intent myIntent = new Intent(MultiWiiBT_menu.this,
+					BTDeviceList.class);
+			// MultiWiiBT_menu.this.startActivity(myIntent);
+			MultiWiiBT_menu.this.startActivityForResult(myIntent,
+					REQUEST_CONNECT_DEVICE);
+			// newGame();
+			return true;
+			/*
+			 * case R.id.discoverable: // showHelp(); Toast.makeText(this,
+			 * "TBD", Toast.LENGTH_LONG).show(); return true;
+			 */
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case REQUEST_CONNECT_DEVICE:
+			// When DeviceListActivity returns with a device to connect
+			if (resultCode == Activity.RESULT_OK) {
+				// Get the device MAC address
+				remote_device_mac = data.getExtras().getString(
+						BTDeviceList.EXTRA_DEVICE_ADDRESS);
+
+				// Save preference in config file
+				prefs.edit().putString("remote_device", remote_device_mac)
+						.commit();
+
+				Toast.makeText(
+						this,
+						"Bluetooth device address " + remote_device_mac
+								+ " saved.", Toast.LENGTH_SHORT).show();
+			}
+			break;
+		case REQUEST_ENABLE_BT:
+			// When the request to enable Bluetooth returns
+			if (resultCode == Activity.RESULT_OK) {
+				// Bluetooth is now enabled, so set up a chat session
+				// setupChat();
+			} else {
+				// User did not enable Bluetooth or an error occured
+				Toast.makeText(this, "Bluetooth was not enabled!",
+						Toast.LENGTH_SHORT).show();
+				finish();
+			}
+		}
+	}
+
+}
